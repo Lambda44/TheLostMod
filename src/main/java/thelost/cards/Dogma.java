f184dab7a31ac703a1401fa80ed6f04719741e61
Lambda44 <brendanlava@gmail.com>
2023-07-13 17:28:36 -0500

    V1.0.6 Dogma Update
    Added Dogma which helps against the Heart, fixed a bug with Tiny House, and buffed Soy Milk slightly.

--- src/main/java/thelost/cards/Dogma.java
+++ src/main/java/thelost/cards/Dogma.java
@@ -1,25 +1,24 @@
 package thelost.cards;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.DexterityPower;
-import com.megacrit.cardcrawl.powers.StrengthPower;
 import thelost.character.TheLost;
-import thelost.powers.TearsPower;
+import thelost.powers.SadOnionPower;
 import thelost.util.CardInfo;
 
 import static thelost.BasicMod.makeID;
 
-public class SoyMilk extends BaseCard {
+public class Dogma extends BaseCard {
     private final static CardInfo cardInfo = new CardInfo(
-            "SoyMilk", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
-            2, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
-            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            "Dogma", //Card ID. Will be prefixed with mod id, so the final ID will be "modID:MyCard" with whatever your mod's ID is.
+            0, //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            TheLost.Enums.CARD_COLOR //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
+            CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardColor.COLORLESS //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or something similar for a basegame character color.
     );
 
     //This is theoretically optional, but you'll want it. The ID is how you refer to the card.
@@ -31,23 +30,27 @@ public class SoyMilk extends BaseCard {
 
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
-    private static final int STR = 2;
-    private static final int STR_UPG = -1;
+    private static final int BLOCK = 4;
+    private static final int BLOCK_UPG = 1;
 
-    public SoyMilk() {
+    private static final int TEARS = 3;
+    private static final int TEARS_UPG = 1;
+
+    public Dogma() {
         super(cardInfo); //Pass the cardInfo to the BaseCard constructor. use true as a 2nd argument to change description
 
-        setMagic(STR, STR_UPG);
+        setBlock(BLOCK, BLOCK_UPG);
+        setMagic(TEARS, TEARS_UPG);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, 3), 3));
-        addToBot(new ApplyPowerAction(p, p, new StrengthPower(p, -this.magicNumber), -this.magicNumber));
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new ApplyPowerAction(p, p, new SadOnionPower(p, this.magicNumber), this.magicNumber));
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new SoyMilk();
+        return new Dogma();
     }
 }
--- src/main/java/thelost/relics/SoulOfTheLost.java
+++ src/main/java/thelost/relics/SoulOfTheLost.java
@@ -1,11 +1,14 @@
 package thelost.relics;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.powers.BufferPower;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.rooms.EventRoom;
+import thelost.cards.Dogma;
 import thelost.character.TheLost;
 
 import static thelost.BasicMod.makeID;
@@ -40,6 +43,16 @@ public class SoulOfTheLost extends BaseRelic{
     public void onEnterRoom(AbstractRoom room) {
         if(AbstractDungeon.player.maxHealth > 1) {
             AbstractDungeon.player.maxHealth = 1;
+        }
+        if(AbstractDungeon.player.currentHealth > 1) {
+            AbstractDungeon.player.currentHealth = 1;
+        }
+    }
+
+    public void atBattleStartPreDraw() {
+        if (AbstractDungeon.floorNum == 55) {
+            this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            this.addToBot(new MakeTempCardInHandAction(new Dogma(), 1, false));
         }
     }
 
